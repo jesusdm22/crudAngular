4852bcae2bc15a1ce95b7dd05f38778dc50ab116
Jesús Díaz Muñoz <jesusdm22@hotmail.com>
2019-04-08 13:22:11 +0200

    act

--- crudAngular/src/app/app.component.ts
+++ crudAngular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {} from './models/empleado';
+import {} from './models/empleado.ts';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
