7fb224927682f5a22a6863e825ad4ba232d996f4
Jesús Díaz Muñoz <jesusdm22@hotmail.com>
2019-04-08 13:20:14 +0200

    actualizacion app component.ts

--- crudAngular/src/app/app.component.ts
+++ crudAngular/src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import {} from 'models/empleado';
+import {} from './models/empleado';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
