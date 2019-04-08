21b490c9448ee3a87d89fe1b6bdea1fea30553a4
Jesús Díaz Muñoz <jesusdm22@hotmail.com>
2019-04-08 13:25:15 +0200

    actualizacion

--- crudAngular/src/app/app.component.ts
+++ crudAngular/src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
-import {} from './models/empleado';
+import { Empleado } from './models/empleado';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  empleadosArray: Empleado[] = [
+  empleadosArray: empleado[] = [
     {id: 1, nombre: 'Jesus', pais:'España'},
     {id: 2, nombre: 'Cristian', pais:'Somalia'},
     {id: 3, nombre: 'Yung Lee', pais:'China'}
